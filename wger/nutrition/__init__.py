69dd1619940669fcd2b4948ef2b64abcab857b9d
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-01-17 15:43:12 +0300

    [Feature #154239873] Set default application configuration

--- wger/nutrition/__init__.py
+++ wger/nutrition/__init__.py
@@ -15,7 +15,9 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Workout Manager.  If not, see <http://www.gnu.org/licenses/>.
 
-
 from wger import get_version
 
 VERSION = get_version()
+
+default_app_config = 'wger.nutrition.apps.NutritionConfig'
+
