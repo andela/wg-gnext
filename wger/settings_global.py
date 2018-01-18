fab84cbfd499db5b4e4ba345c2175b0669e52d69
geofrocker <geofrocker2@gmail.com>
2018-01-18 16:09:57 +0300

    [ch #154434695] Add the static url

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -229,8 +229,10 @@ LOCALE_PATHS = (os.path.join(SITE_ROOT, 'locale'), )
 
 FLAVOURS_STORAGE_BACKEND = 'session'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static-collected')
+STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #
 # Logging
