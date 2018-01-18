a85558f52c2447974cee76ebc2709a19008408d5
geofrocker <geofrocker2@gmail.com>
2018-01-18 14:43:38 +0300

    [ch #154434695] Revert to the original settings folder

--- wger/wsgi.py
+++ wger/wsgi.py
@@ -17,7 +17,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "../settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
