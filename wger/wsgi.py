809273eca758996dc647ee7c4e0764ac19e237d3
geofrocker <geofrocker2@gmail.com>
2018-01-18 12:37:58 +0300

    [ch #154434695] Add waitress as a server for the application

--- wger/wsgi.py
+++ wger/wsgi.py
@@ -15,6 +15,7 @@ framework.
 """
 import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
@@ -22,6 +23,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
