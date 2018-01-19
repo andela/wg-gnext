ca113cde1fedaf7803b01bb4b7e5c7ea1fc00952
geofrocker <geofrocker2@gmail.com>
2018-01-19 13:04:44 +0300

    [ch #154434695] Remove static files directory

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -231,7 +231,6 @@ LOCALE_PATHS = (os.path.join(SITE_ROOT, 'locale'), )
 FLAVOURS_STORAGE_BACKEND = 'session'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "../static")]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static-collected')
 
 #
