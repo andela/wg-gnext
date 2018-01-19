748364c4fd1069c8dd5fb9346aafec628d202e56
geofrocker <geofrocker2@gmail.com>
2018-01-19 16:34:37 +0300

    [ch #154434695] Add project root to point to settings.py

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import sys
+import os
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 from django.core.management import execute_from_command_line
 
 from tasks import (
@@ -12,7 +14,7 @@ if __name__ == "__main__":
 
     # If user passed the settings flag ignore the default wger settings
     if not any('--settings' in s for s in sys.argv):
-        setup_django_environment(get_user_config_path('wger', 'settings.py'))
+        setup_django_environment(os.path.join(PROJECT_ROOT, 'settings.py'))
 
     # Alternative to above
     # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
