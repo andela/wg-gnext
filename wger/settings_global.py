b30217584543676a16e8a07cbb07cd86e4de9018
geofrocker <geofrocker2@gmail.com>
2018-01-19 12:59:11 +0300

    [ch #154434695] Add static files directory

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -229,7 +229,9 @@ LOCALE_PATHS = (os.path.join(SITE_ROOT, 'locale'), )
 
 FLAVOURS_STORAGE_BACKEND = 'session'
 
-STATIC_ROOT = ''
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "../static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static-collected')
 
 #
 # Logging
