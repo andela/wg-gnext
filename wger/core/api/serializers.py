ab7737ff58bce8ab3cb348ef8301956badd18c34
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-01-25 12:19:25 +0300

    [Feature 154239875]Add serializer for auth user model

--- wger/core/api/serializers.py
+++ wger/core/api/serializers.py
@@ -15,13 +15,63 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Workout Manager.  If not, see <http://www.gnu.org/licenses/>.
 
+from django.contrib.auth.models import User
+from django.utils import translation
 from rest_framework import serializers
-
 from wger.core.models import (UserProfile, Language, DaysOfWeek, License,
-                              RepetitionUnit, WeightUnit)
+                              RepetitionUnit, WeightUnit, RestUser)
+from wger.config.models import GymConfig, GymUserConfig
 
 
-class UserprofileSerializer(serializers.ModelSerializer):
+
+class AuthUserSerializer(serializers.ModelSerializer):
+    '''
+    Default auth user serializer
+    '''
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username'])
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        # Pre-set some values of the user's profile
+        language = Language.objects.get(
+            short_name=translation.get_language())
+        user.userprofile.notification_language = language
+
+        # Set default gym, if needed
+        gym_config = GymConfig.objects.get(pk=1)
+        if gym_config.default_gym:
+            user.userprofile.gym = gym_config.default_gym
+
+            # Create gym user configuration object
+            config = GymUserConfig()
+            config.gym = gym_config.default_gym
+            config.user = user
+            config.save()
+
+        user.userprofile.save()
+
+        # Add tracking of user created via REST API and the user/app who made the request
+        # first get the user making the request.
+        creator = getattr(self.context['request'], 'user')
+        rest_user = RestUser()
+        rest_user.user = user
+        rest_user.created_by = creator
+        rest_user.save()
+
+        return user
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
     '''
     Workout session serializer
     '''
