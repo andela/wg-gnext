e75eae149ba16910f4915e60f3d865a89d100798
silverjimmy2 <silverjimmy2@gmail.com>
2018-01-26 18:22:05 +0300

    [ft #154239891] Add google as a Login service

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -135,6 +135,7 @@ MIDDLEWARE_CLASSES = (
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.github.GithubOAuth2', # <--
     'social_core.backends.twitter.TwitterOAuth', # <--
+    'social_core.backends.google.GoogleOAuth2', # <--
     'social_core.backends.facebook.FacebookOAuth2', # <--
     'django.contrib.auth.backends.ModelBackend',
     'wger.utils.helpers.EmailAuthBackend'
@@ -404,6 +405,9 @@ SOCIAL_AUTH_GITHUB_SECRET = ''
 
 SOCIAL_AUTH_TWITTER_KEY = ''
 SOCIAL_AUTH_TWITTER_SECRET = ''
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = ''
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = ''
 
 
 SOCIAL_AUTH_PIPELINE = (
